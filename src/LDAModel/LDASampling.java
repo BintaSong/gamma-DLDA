368f51605a54e556650f699fc206c5c140b9cb18
binta <bintasong@gmai.com>
2015-04-11 19:55:01 +0800

    "commit again,git!"

--- src/LDAModel/LDASampling.java
+++ src/LDAModel/LDASampling.java
@@ -12,8 +12,8 @@ import FollowProcess.FollowWork;
  * 
  * */
 public class LDASampling{
-	public static int topicNumOfAux = 25;
-	public static int topicNumOfTar = 10;
+	public static int topicNumOfAux = 15;
+	public static int topicNumOfTar = 15;
 	public static int iteration = 200;
 	public static double beta = 0.01;
 	public static double gama_small = 0.2;
@@ -24,11 +24,11 @@ public class LDASampling{
 		
 		Corpus corpus =new Corpus(topicNumOfAux,topicNumOfAux,50.0/(topicNumOfAux+topicNumOfTar),beta,gama_big,gama_small);
 		System.out.println("add Auxiliary document to memory begin ...");
-		corpus.addDoc("C:/Users/binta/Desktop/lda_java/gama-DLDA/Data/Auxiliary",auxFlag);
+		corpus.addDoc("C:/Users/binta/Desktop/lda_java/gamma-DLDA/Data/Auxiliary",auxFlag);
 		System.out.println("add Auxiliary document to memory done.");
 		corpus.MA = corpus.M;//记录当前的辅助文档数目！
 		System.out.println("add Target document to memory begin ...");
-		corpus.addDoc("C:/Users/binta/Desktop/lda_java/gama-DLDA/Data/Target",tarFlag);
+		corpus.addDoc("C:/Users/binta/Desktop/lda_java/gamma-DLDA/Data/Target",tarFlag);
 		System.out.println("add Target document to memory done.");
 		System.out.printf("the number of words: %d\n",corpus.V);
 		
@@ -38,7 +38,7 @@ public class LDASampling{
 		System.out.println("iteration...");
 		corpus.sampling(iteration);
 		System.out.println("iteration done.");
-		FollowWork fw = new FollowWork(corpus,"C:/Users/binta/Desktop/lda_java/gama-DLDA/Data/LDAResults/document-topic.txt","C:/Users/binta/Desktop/lda_java/gama-DLDA/Data/LDAResults/topic-word.txt","C:/Users/binta/Desktop/lda_java/gama-DLDA/Data/LDAResults/theta.txt");
+		FollowWork fw = new FollowWork(corpus,"C:/Users/binta/Desktop/lda_java/gamma-DLDA/Data/LDAResults/document-topic.txt","C:/Users/binta/Desktop/lda_java/gamma-DLDA/Data/LDAResults/topic-word.txt","C:/Users/binta/Desktop/lda_java/gamma-DLDA/Data/LDAResults/theta.txt");
 		fw.sortMKandSave();
 		fw.sortKWandSave();
 		fw.writeTheta();
